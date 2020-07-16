fb61f8566a22aa8bc107e789e09d1e3b91f1b98b
Mody Sleiman <modysleiman@gmail.com>
2020-07-15 22:16:47 -0700

    added firebase through yarn, and firebase files

---
+++ haven/src/Components/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './CSS/App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Haven</h1>
+      <p>Convenient Stops. Safe Rides. Monumental Experiences 🚲</p>
+      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      <p><small>Campbell, Ca</small></p>
+      <p><small>July 2020</small></p>
+    </div>
+  );
+}
+
+export default App;
