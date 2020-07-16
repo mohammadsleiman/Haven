f6b16460f876e4f8891393dd3090047bb13685eb
Mody Sleiman <modysleiman@gmail.com>
2020-07-15 21:14:28 -0700

    Setup React Project

---
+++ haven/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Haven</h1>
+      <p>Convenient Stops. Safe Rides. Monumental Experiences 🚲</p>
+      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+      <p><small>Campbell, Ca</small></p>
+      <p><small>July 2020</small></p>
+    </div>
+  );
+}
+
+export default App;
