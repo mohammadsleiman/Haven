10b9f007f164d34c1396255025f747644dc6b1f3
Mody Sleiman <modysleiman@gmail.com>
2020-07-16 23:17:56 -0700

    Added Grids to separate page elements and added card elements

--- haven/src/Components/App.js
+++ haven/src/Components/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
+import "./CSS/App.css";
+import { green } from "@material-ui/core/colors";
+import DestinationListContainer from "./DestinationList/DestinationListContainer";
 
-import './CSS/App.css';
+const useStyles = makeStyles((theme) => ({
+  gridStyle: {
+    borderStyle: "solid",
+  },
+  navBarStyle: {
+    backgroundColor: green[500],
+    borderStyle: "solid",
+  },
+}));
 
 function App() {
-
+  const classes = useStyles();
   return (
-    <div>
-      <h1>Haven</h1>
-      <p>Convenient Stops. Safe Rides. Monumental Experiences 🚲</p>
-      
+    <Grid className={classes.gridStyle} container direction="column">
+      <Grid className={classes.navBarStyle} container item direction="row">
+        <h1>Haven</h1>
+        <p>Convenient Stops. Safe Rides. Monumental Experiences 🚲</p>
+      </Grid>
+
+      <Grid className={classes.gridStyle} container item direction="row">
+        <DestinationListContainer />
+
+        <Grid item className={classes.gridStyle} xs={8}>
+          <h1> I am the maps</h1>
+        </Grid>
+      </Grid>
+
       <br></br>
       <br></br>
       <br></br>
@@ -29,10 +51,14 @@ function App() {
       <br></br>
       <br></br>
       <br></br>
-      
-      <p><small>Campbell, Ca</small></p>
-      <p><small>July 2020</small></p>
-    </div>
+
+      <p>
+        <small>Campbell, Ca</small>
+      </p>
+      <p>
+        <small>July 2020</small>
+      </p>
+    </Grid>
   );
 }
 
