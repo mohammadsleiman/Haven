c5d7a739109f2c3fe352dd35d67f4525eddb2e22
Mody Sleiman <modysleiman@gmail.com>
2020-08-14 11:48:21 -0700

    setup backend

--- haven/src/Components/BodyComponents/PreviewCard.js
+++ haven/src/Components/BodyComponents/PreviewCard.js
@@ -6,51 +6,96 @@ import {
   CardMedia,
   makeStyles,
   CardActionArea,
+  Shadows,
 } from "@material-ui/core";
 import pic1 from "./sjsu.png";
 import { grey } from "@material-ui/core/colors";
+import { shadows } from "@material-ui/system";
+
+// OLD USESTYLES
+// const useStyles = makeStyles(() => ({
+//   previewCardMediaStyle: {
+//     height: 175,
+//     width: 325,
+//   },
+//   previewCardStyle: {
+//     borderRadius: 45,
+//   },
+//   previewCardSubTypographyStyle: {
+//     color: grey[500],
+//     marginLeft: "20px",
+//   },
+//   previewCardMainTypographyStyle: { marginLeft: "20px" },
+// }));
 
 const useStyles = makeStyles(() => ({
   previewCardMediaStyle: {
-    height: 175,
-    width: 325,
-  },
-  previewCardStyle: {
+    height: 225,
+    width: 300,
     borderRadius: 45,
   },
+  previewCardStyle: {},
   previewCardSubTypographyStyle: {
     color: grey[500],
-    marginLeft: "20px",
+    marginLeft: "-17px",
   },
-  previewCardMainTypographyStyle: { marginLeft: "20px" },
+  previewCardMainTypographyStyle: { marginLeft: "-17px" },
 }));
 
 export default function PreviewCard(props) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.previewCardStyle}>
+    <Card className={classes.previewCardStyle} elevation={0}>
       <CardActionArea>
-        <CardMedia
-          className={classes.previewCardMediaStyle}
-          image={props.havenData.image}
-          title="picture"
-        />
         <CardContent>
-          <Typography
-            variant="body1"
-            className={classes.previewCardSubTypographyStyle}
-          >
-            {props.havenData.neighborhood} · {props.havenData.time} 🚲
-          </Typography>
           <Typography
             variant="h6"
             className={classes.previewCardMainTypographyStyle}
           >
             {props.havenData.name}
           </Typography>
+          <Typography
+            variant="body1"
+            className={classes.previewCardSubTypographyStyle}
+          >
+            {props.havenData.neighborhood} · {props.havenData.time} 🚲
+          </Typography>
         </CardContent>
+
+        <CardMedia
+          className={classes.previewCardMediaStyle}
+          image={props.havenData.image}
+          title="picture"
+        />
       </CardActionArea>
     </Card>
   );
+
+  //OLD CARD
+  // return (
+  //   <Card className={classes.previewCardStyle}>
+  //     <CardActionArea>
+  //       <CardMedia
+  //         className={classes.previewCardMediaStyle}
+  //         image={props.havenData.image}
+  //         title="picture"
+  //       />
+  //       <CardContent>
+  //         <Typography
+  //           variant="body1"
+  //           className={classes.previewCardSubTypographyStyle}
+  //         >
+  //           {props.havenData.neighborhood} · {props.havenData.time} 🚲
+  //         </Typography>
+  //         <Typography
+  //           variant="h6"
+  //           className={classes.previewCardMainTypographyStyle}
+  //         >
+  //           {props.havenData.name}
+  //         </Typography>
+  //       </CardContent>
+  //     </CardActionArea>
+  //   </Card>
+  // );
 }
